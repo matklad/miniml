d8e6a6ec95b609835e89b2b3bf3f4300064c3fb1
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-03-10 22:42:10 +0300

    add non iteractive mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate miniml;
 
 use std::io::prelude::*;
+use std::fs::File;
 use std::io;
 
 fn readline(ps: &str, buffer: &mut String) {
@@ -23,22 +24,43 @@ fn repl<F: Fn(&str) -> String>(f: F) {
     }
 }
 
+fn execute(expr: &str) -> String {
+    let expr = match miniml::parse(expr) {
+        Err(e) => return format!("Parse error: {:?}", e),
+        Ok(e) => e,
+    };
+    let t = match miniml::typecheck(&expr) {
+        Err(e) => return format!("Type error: {:?}", e),
+        Ok(t) => t,
+    };
+    let program = miniml::compile(&expr);
+    let mut machine = miniml::Machine::new(&program);
+    let result = match machine.exec() {
+        Err(e) => return format!("{}", e.message),
+        Ok(x) => x,
+    };
+    format!("{}", result)
+
+}
+
+fn start_repl() {
+    repl(execute);
+}
+
+fn exec_file(path: &str) {
+    let mut buffer = String::new();
+    let mut file = File::open(path).unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    let result = execute(&buffer);
+    println!("{}", result);
+}
+
 fn main() {
-    repl(|expr| {
-        let expr = match miniml::parse(expr) {
-            Err(e) => return format!("Parse error: {:?}", e),
-            Ok(e) => e,
-        };
-        let t = match miniml::typecheck(&expr) {
-            Err(e) => return format!("Type error: {:?}", e),
-            Ok(t) => t,
-        };
-        let program = miniml::compile(&expr);
-        let mut machine = miniml::Machine::new(&program);
-        let result = match machine.exec() {
-            Err(e) => return format!("{}", e.message),
-            Ok(x) => x,
-        };
-        format!("{}", result)
-    });
+    let mut args = std::env::args();
+    args.next().unwrap();
+    if let Some(file) = args.next() {
+        exec_file(&file)
+    } else {
+        start_repl()
+    }
 }
